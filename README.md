8a2bcc58b2fee862d73f4aca93d092e06f3fc4c2
iuliia <yulia.pi@gmail.com>
2018-07-25 20:10:18 -0700

    Add styled component to App.js. Updated readme.md

---
+++ README.md
@@ -0,0 +1,30 @@
+#Crazy flies and other bites 
+Collection of small tasks that will be integrated into the final project "Jar of memories"
+
+Completed: Crazy flies [demo](http://yuliapi.github.io/projects/crazy-flies/index.html)
+
+Under development: Shake the jar
+## Getting started
+### Prerequisites
+You need npm to be installed globally
+
+### Install 
+Install requirements:
+```
+npm install
+```
+### Start
+Start local development server execute:
+```
+npm start
+```
+Compile into static web resource (public folder):
+```
+npm run build
+```
+## Build with:
+- React.js
+- GSAP
+
+## Author
+Iuliia Pishchulina
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {connect} from "react-redux";
 import 'normalize.css';
 import './App.css';
 import styled from 'styled-components';
@@ -10,29 +9,60 @@ import {
     HashRouter
 } from "react-router-dom";
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCrow } from '@fortawesome/free-solid-svg-icons'
-
-
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faCrow} from '@fortawesome/free-solid-svg-icons'
 
 import Home from "./components/Home";
 import CrazyFlies from "./components/Crazy-flies";
 import Shake from "./components/Shake-the-jar";
 
-library.add(faCrow)
+library.add(faCrow);
+
+const StyledDiv = styled.div`
+   & ul {
+      background-color: #111;
+      padding: 0;
+      border-bottom: 2px solid red;
+   }
+   
+   & ul li {
+      display: inline;
+      list-style-type: none;
+   }
+   
+   & ul li a {
+      display: inline-block;
+      color: #fff;
+      font-size: 1.2em;
+      font-weight: bold;
+      padding: 10px 20px;
+      text-decoration: none;
+      
+      &:hover {
+        color: red;
+      }
+      
+      &.active {
+        background-color: red;
+        &:hover {
+           color: #111;
+        }
+      }
+   }
+`;
+
 class App extends Component {
     render() {
-
-
         return (
             <HashRouter>
                 <div>
-                    <ul className="main-nav">
-                        <li><NavLink exact to="/">Home</NavLink></li>
-                        <li><NavLink to="/crazy-flies">Crazy flies</NavLink></li>
-                        <li><NavLink to="/shake-jar">Shake the Jar</NavLink></li>
-                    </ul>
+                    <StyledDiv>
+                        <ul className="main-nav">
+                            <li><NavLink exact to="/">Home</NavLink></li>
+                            <li><NavLink to="/crazy-flies">Crazy flies</NavLink></li>
+                            <li><NavLink to="/shake-jar" className='link-disabled'>Shake the Jar</NavLink></li>
+                        </ul>
+                    </StyledDiv>
                     <div className="content">
                         <Route exact path="/" component={Home}/>
                         <Route path="/crazy-flies" component={CrazyFlies}/>
